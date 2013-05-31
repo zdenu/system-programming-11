645494afa4e306e5a6d58e50bcd9d8fedcd00de5
Laogram <Laogram@gmail.com>
2013-05-31 07:57:38 +0000

    add touch listener.

--- WolframAlphaAPI/TouchListener.h
+++ WolframAlphaAPI/TouchListener.h
@@ -10,6 +10,10 @@
 #include "atomic_value.h"
 #include "thread.h"
 
+#define INVALID_SOCKET	-1
+
+
+typedef bool (*TOUCH_EVENT)(struct input_event& ev);
 
 class TouchListener
 {
@@ -19,12 +23,22 @@ public:
 	
 public:
 	bool initialize(void);
-	
+	bool destroy(void);
 	
 private:
 	void* touchThread(Thread<TouchListener>*, void* );
 	
+	
+	static bool handleEV_SYN(struct input_event& ev);
+	static bool handleEV_KEY(struct input_event& ev);
+	static bool handleEV_ABS(struct input_event& ev);
+	
 private:
 	AtomicValue<bool>	 isThreadRunning;
 	Thread<TouchListener> thread;
+	
+	TOUCH_EVENT	eventHandler[EV_MAX];
+	
+	
+	int fd;
 };
