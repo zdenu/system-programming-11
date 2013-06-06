bd6337f2673f6f598b61c24909cd504f2db0c693
Laogram@gmail.com <Laogram@gmail.com@a30425d8-0e2c-bde6-4f50-4b1b61f36ff3>
2013-06-06 04:34:18 +0000

    add camculator.

---
+++ trunk/camculator/camculator/touch.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/input.h>
+#include "touch.h"
+
+typedef struct touchEventArea
+{
+	int x;
+	int y;
+	int w;
+	int h;
+	int enable;
+} touchEventArea;
+
+touchEventArea eventArray[MAX_TOUCH_EVENT];
+int touch_evnet_cnt = 0;
+
+void initTouchevent(){
+	int i;
+	for(i=0;i<MAX_TOUCH_EVENT;i++)
+			eventArray[i].enable == 0;
+}
+
+int addTouchevent(int x, int y, int width, int height){
+	int i=0;
+	for(i=0;i<MAX_TOUCH_EVENT;i++){
+		if(eventArray[i].enable == 0) {
+			eventArray[i].x = x;
+			eventArray[i].y = y;
+			eventArray[i].w = width;
+			eventArray[i].h = height;
+			eventArray[i].enable = 1;
+			if(touch_evnet_cnt<i+1){
+				touch_evnet_cnt = i+1;
+			}
+			return i;
+		}
+	}
+}
+
+void removeTouchevent(int num){
+	eventArray[num].enable = 1;
+}
+
+void removeAllTouchevent(){
+	int i;	
+	for(i=0;i<touch_evnet_cnt;i++){
+		eventArray[i].enable = 1;
+	}
+	touch_evnet_cnt = 0;
+}
+
+int touch(){
+	int i,j;
+	size_t read_bytes;
+	int bx=0,by=0;
+	int x=0,y=0;
+	struct input_event event_buf[EVENT_BUF_NUM];
+	char *device;
+	int event_fd = -1;
+	device = "/dev/input/event0";
+
+	// input device open
+	if ((event_fd = open(device, O_RDONLY)) < 0)
+	{
+		printf("%s: open error", device);
+		exit(1);
+	}
+	printf("open : [%s] \n",device);   // raoudi
+	while (1)
+	{
+		read_bytes = read(event_fd, event_buf, (sizeof(struct input_event)*EVENT_BUF_NUM) );
+		if( read_bytes < sizeof(struct input_event) )
+		{
+			printf("%s: read error", device);
+			exit(1);
+		}
+		for( i=0; i<(read_bytes/sizeof(struct input_event)); i++ )
+		{
+			switch( event_buf[i].type )
+			{
+			case EV_SYN:
+				break;
+			case EV_ABS:
+				switch (event_buf[i].code)
+				{
+				case ABS_X:
+					if(abs(event_buf[i].value-bx) > 30 ) {
+						x = (float)event_buf[i].value*0.32;
+						bx = event_buf[i].value;
+						//x = x;
+					}
+					break;
+				case ABS_Y:
+					if(abs(event_buf[i].value-by) > 30) {
+						y = (float)event_buf[i].value*0.24;
+						y = 240 - y;
+						by=event_buf[i].value;
+					}
+					break;
+				case ABS_PRESSURE:
+					//printf("touch : %d, %d\n", x,y);
+					for(j=0;j<touch_evnet_cnt;j++){
+						//printf("%d~%d\n",eventArray[j].x,eventArray[j].x+eventArray[j].w);
+						if(x > eventArray[j].x && x < eventArray[j].x+eventArray[j].w && y > eventArray[j].y && y < eventArray[j].y+eventArray[j].h)
+						{
+							printf("event occar! : %d\n", j);
+							return j;
+						}
+					}
+					break;
+				default:
+					printf("Touch Unknown: type %d, code %d, value %d\n",
+							event_buf[i].type,
+							event_buf[i].code,
+							event_buf[i].value);
+					break;
+				}
+				break;
+			default:
+				printf("Unknown: type %d, code %d, value %d\n",
+						event_buf[i].type,
+						event_buf[i].code,
+						event_buf[i].value);
+				break;
+			}
+
+		}
+	}
+
+}
+
+int touch_pause(){
+	size_t read_bytes;
+	struct input_event event_buf[EVENT_BUF_NUM];
+	char *device;
+	int event_fd = -1;
+	int i;
+	device = "/dev/input/event0";
+
+	// input device open
+	if ((event_fd = open(device, O_RDONLY)) < 0)
+	{
+		printf("%s: open error", device);
+		exit(1);
+	}
+	printf("open : [%s] \n",device);   // raoudi
+	while (1)
+	{
+		read_bytes = read(event_fd, event_buf, (sizeof(struct input_event)*EVENT_BUF_NUM) );
+		if( read_bytes < sizeof(struct input_event) )
+		{
+			printf("%s: read error", device);
+			exit(1);
+		}
+		for( i=0; i<(read_bytes/sizeof(struct input_event)); i++ )
+		{
+			switch( event_buf[i].type )
+			{
+			case EV_SYN:
+				break;
+			case EV_ABS:
+				switch (event_buf[i].code)
+				{
+				case ABS_X:
+					break;
+				case ABS_Y:
+					break;
+				case ABS_PRESSURE:
+					return 1;
+					break;
+				default:
+					printf("Touch Unknown: type %d, code %d, value %d\n",
+							event_buf[i].type,
+							event_buf[i].code,
+							event_buf[i].value);
+					break;
+				}
+				break;
+			default:
+				printf("Unknown: type %d, code %d, value %d\n",
+						event_buf[i].type,
+						event_buf[i].code,
+						event_buf[i].value);
+				break;
+			}
+
+		}
+	}
+
+}
