c5ae4d3ad3af35475a609609c9ff746cadd20acb
passa8837@gmail.com <passa8837@gmail.com@a30425d8-0e2c-bde6-4f50-4b1b61f36ff3>
2013-06-10 11:41:09 +0000

    mp3

---
+++ trunk/camculator_source/camculator/mp3.h
@@ -0,0 +1,8 @@
+void soundSetVolume(int updown, int num);
+int MP3_initialize();
+void MP3_play(char *path);
+void MP3_stop();
+void MP3_pause();
+void soundSetVolume(int updown, int num);
+int soundGetVolume();
+
