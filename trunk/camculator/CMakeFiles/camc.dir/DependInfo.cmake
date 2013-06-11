ae1f4656f92209d4d7c6db37d5b717d6a6dd1a07
Laogram@gmail.com <Laogram@gmail.com@a30425d8-0e2c-bde6-4f50-4b1b61f36ff3>
2013-06-11 10:16:08 +0000

    add all

---
+++ trunk/camculator/CMakeFiles/camc.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/mp3.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/mp3.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/giflib/dgif_lib.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/giflib/dgif_lib.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/giflib/egif_lib.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/giflib/egif_lib.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/giflib/gif_err.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/giflib/gif_err.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/giflib/gif_font.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/giflib/gif_font.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/giflib/gif_hash.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/giflib/gif_hash.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/giflib/gifalloc.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/giflib/gifalloc.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/giflib/quantize.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/giflib/quantize.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/fontinfo.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/fontinfo.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gx.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gx.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gxbdf.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gxbdf.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gxbmp.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gxbmp.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gxfile.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gxfile.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gximagelist.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gximagelist.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gxjpg.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gxjpg.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gxlayer.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gxlayer.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gxmosaic.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gxmosaic.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gxpanel.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gxpanel.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/gxpng.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/gxpng.c.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/gxlib/ksc5601.c" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/gxlib/ksc5601.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/HttpConnector.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/HttpConnector.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/WolframAlphaManager.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/WolframAlphaManager.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/camculator.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/camculator.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/camera.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/camera.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/crop.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/crop.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/edit.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/edit.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/home.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/home.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/keypad.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/keypad.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/labeling.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/labeling.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/main.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/main.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/opencv.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/opencv.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/result.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/result.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/state.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/state.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/touch.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/touch.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/camculator/util.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/camculator/util.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/opencv/BlobLabeling.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/opencv/BlobLabeling.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/tinyxml/tinystr.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/tinyxml/tinystr.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/tinyxml/tinyxml.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/tinyxml/tinyxml.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/tinyxml/tinyxmlerror.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/tinyxml/tinyxmlerror.cpp.o"
+  "/Users/laogram/Project/private/system-programming-11/trunk/camculator/tinyxml/tinyxmlparser.cpp" "/Users/laogram/Project/private/system-programming-11/trunk/camculator/CMakeFiles/camc.dir/tinyxml/tinyxmlparser.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "./camculator"
+  "./gxlib"
+  "./giflib"
+  "./opencv"
+  "/usr/local/arm/arm-none-linux-gnueabi/include"
+  "/usr/local/arm/arm-none-linux-gnueabi/include/opencv"
+  "/usr/local/arm/arm-none-linux-gnueabi/include/opencv2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
