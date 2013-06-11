16f45eabac425a355242ef1eba8be49fe55ad21f
passa8837 <passa8837@gmail.com>
2013-06-11 03:30:52 +0000

    /camculator_source/led edit

--- camculator_source/camculator/led_bz_sw.c
+++ camculator_source/camculator/led_bz_sw.c
@@ -16,13 +16,11 @@
 
 
 #define ADDRESSOFLED		0x14805000	// led addr.
-#define ADDRESSOFSW		0x14802000	//  sw addr. 3bit
 #define ADDRESSOFBZ		0x14808000 // buzzer addr	
 
 
 static int fd_mem;
 unsigned char *pled;
-unsigned char *psw;
 unsigned char *pbz;
 
 int initialize()
@@ -32,11 +30,10 @@ int initialize()
 		return -1;
     }        
 
-    pled = mmap(NULL,1,PROT_WRITE, MAP_SHARED, fd_mem, ADDRESSOFLED);
-	psw = mmap(NULL, 1,PROT_WRITE,MAP_SHARED,fd_mem,ADDRESSOFSW);
+   pled = mmap(NULL,1,PROT_WRITE, MAP_SHARED, fd_mem, ADDRESSOFLED);
 	pbz = mmap(NULL, 1,PROT_WRITE,MAP_SHARED,fd_mem,ADDRESSOFBZ);
 
-    if( (int)pled < 0 || (int)psw<0 || (int)pbz<0){
+    if( (int)pled < 0 || (int)pbz<0){
 		close(fd_mem);
       	perror("mmap error\n");
        	return -1;
@@ -53,45 +50,25 @@ void LED_OFF(unsigned char val){
 	*pled = 0x00;
 }
 
-void LED_close(){
-	munmap(pled,1);
-}
-
-unsigned char SW_read(){
-	return (*psw);
-}
-
-void SW_close(){
-	munmap(psw,1);
-}
-
 void Buzzer(int time){
 	*pbz = 0x01;
 	usleep(time*1000);
 	*pbz = 0x00;
 }
 
-void BZ_close(){
+void close(){
+	munmap(pled,1);
 	munmap(pbz,1);
-}
-
-void MEM_close(){
 	close(fd_mem);
 }
 
-static int dev; 
 int main(void) 
 { 
  initialize();
 
  Buzzer(100);
  LED_ON(0xFF);
-
- while(1) {
-		printf("%d %d %d\n",SW_read()&0x01,(SW_read()&0x02)>>1,(SW_read()&0x04)>>2);
-		LED_ON(SW_read());
-  }
- BZ_close();
- MEM_close();
+ 
+ close();
  return 0; 
 }
